39445729c3a5ab4585b2d8225d32ebb884b288b9
Marcos <marcos.cvford@gmail.com>
2024-11-21 15:42:52 -0600

    Creacion de portafolio

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const menuIcon = document.querySelector(".menu-icon");
-const sidebar = document.querySelector(".sidebar");
+const date = document.getElementById('date')
+const setCurrentYear = () => {
+	const currentYear = new Date().getFullYear()
+	date.textContent = currentYear
+}
+setCurrentYear()
 
-menuIcon.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-});
+// hamburger menu
+const hamburger = document.querySelector('.hamburger')
+const navList = document.querySelector('.nav-list')
+const socials = document.querySelector('.social-nav')
+const navLinks = document.querySelectorAll('.nav-links')
+
+const toggleMobileMenu = () => {
+	hamburger.classList.toggle('open')
+	navList.classList.toggle('open')
+	socials.classList.toggle('open')
+	document.body.classList.toggle('open')
+}
+
+navLinks.forEach(link => link.addEventListener('click', toggleMobileMenu))
+hamburger.addEventListener('click', toggleMobileMenu)
+
+// initialize aos (library for scroll animation)
+AOS.init()
